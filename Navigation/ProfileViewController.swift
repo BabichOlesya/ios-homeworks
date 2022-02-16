9bb2d0b8266db9636cf26833952d4069cd7f15b9
Olesya Babich <babich.lesya@gmail.com>
2022-02-17 01:37:12 +0300

    Add Profile.storyboard, ProfileViewController.swift

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Олеся Бабич on 17.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
