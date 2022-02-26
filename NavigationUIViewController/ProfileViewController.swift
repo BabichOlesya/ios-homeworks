9e17d1f2316cbfdcc2ef8343293a2fb3c48c833c
Olesya Babich <babich.lesya@gmail.com>
2022-02-27 00:35:43 +0300

    add homework 2

---
+++ NavigationUIViewController/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  NavigationUIViewController
+//
+//  Created by Олеся Бабич on 25.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
